b39671cfe89a033e6d9753a22179d20e282b43f0
dannyl <danilovlaurent@pursuit.org>
2020-03-05 20:23:56 -0500

    create posts routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRouter = require("./routes/users/usersRoutes");
+const postsRouter = require("./routes/posts/postRoutes");
 const port = 3001;
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api", userRouter);
+app.use("/api", postsRouter);
 
 app.listen(port, () => {
   console.log(`Server Is Running On Port:${port}`);
--- backend/routes/posts/postRoutes.js
+++ backend/routes/posts/postRoutes.js
@@ -1,1 +1,20 @@
-// post routes here
+const posts = require("express").Router();
+const {
+  getAllPosts,
+  getPostById,
+  getTagOfPost,
+  getPostByTag,
+  getPicturesOfPost,
+  createPost,
+  deletePost
+} = require("../../queries/posts/postQueries");
+
+posts.get("/posts", getAllPosts);
+posts.get("/posts/:id", getPostById);
+posts.get("posts/:id/:tag", getTagOfPost);
+posts.get("posts/tags/:tagName", getPostByTag);
+posts.get("posts/:id/pictures", getPicturesOfPost);
+posts.post("posts", createPost);
+posts.delete("posts/:id", deletePost);
+
+module.exports = posts;
