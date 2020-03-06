486f8186f0608675fee7c57a72b47e58fcc766ab
Jay Fowler <jcafowler@gmail.com>
2020-03-06 12:06:29 -0500

    Add getAllTags route and query

--- heatcheck/backend/queries/tags.js
+++ heatcheck/backend/queries/tags.js
@@ -1,2 +1,20 @@
 const db = require ("../db/index.js")
 
+const getAllTags = async (req, res, next) => {
+  try {
+    let tags = await db.any ("SELECT * FROM tags")
+    res.status(200).json({
+      status: "success",
+      message: "Retrieved all tags.",
+      payload: tags
+    })
+  } catch(err){
+    res.status(404).json({
+      status: "error",
+      message: "There were no tags found.",
+      payload: null
+    })
+  }
+}
+
+module.exports = {getAllTags}
--- heatcheck/backend/routes/tags.js
+++ heatcheck/backend/routes/tags.js
@@ -1,3 +1,6 @@
-const tags = require ("express").Router()
+const tags = require("express").Router()
+const {getAllTags} = require("../queries/tags.js")
+
+tags.get("/", getAllTags)
 
 module.exports = tags
