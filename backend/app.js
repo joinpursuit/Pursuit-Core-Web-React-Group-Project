84296840cb2556e179ec58990b5638e8b62ceace
dannyl <danilovlaurent@pursuit.org>
2020-03-05 20:05:19 -0500

    create all users routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const userRouter = require("./routes/users/usersRoutes");
 const port = 3001;
 
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use("/api", userRouter);
 
 app.listen(port, () => {
   console.log(`Server Is Running On Port:${port}`);
--- backend/routes/users/usersRoutes.js
+++ backend/routes/users/usersRoutes.js
@@ -1,1 +1,26 @@
-// users routes here
+const users = require("express").Router();
+const {
+  getAllUsers,
+  getUserByid,
+  getUserByEmail,
+  getPostByUser,
+  getPostByUser,
+  updateUser,
+  createNewUser,
+  deleteUser
+} = require("../../queries/users/usersQueries");
+
+users.get("/users", getAllUsers);
+
+users.get("/:id", getUserByid);
+
+users.get("users/:email", getUserByEmail);
+
+users.get("users/:id/posts", getPostByUser);
+
+users.patch("users/:id", updateUser);
+
+users.post("/users", createNewUser);
+
+users.delete("users/:id", deleteUser);
+module.exports = users;
