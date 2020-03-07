01a77a1aab88ab38e957a0b57c275c7f2a3c9ef7
Syn Perez <perezsyn@gmail.com>
2020-03-07 15:18:09 -0500

    post queries

--- heatcheck/backend/queries/posts.js
+++ heatcheck/backend/queries/posts.js
@@ -1,7 +1,9 @@
+const db = require('../db/index');
+
 const getposts = async (req, res, next) => {
   try {
-    let posts = await dataBase.any(
-      "SELECT posts.id,posts.image, posts.brand, posts.description, posts.release_date,posts.colorway, posts.user_id, users.user_name FROM posts LEFT JOIN users ON posts.user_id = users.id;"
+    let posts = await db.any(
+      "SELECT *,(SELECT ARRAY_AGG(users.user_name)AS user_name FROM users WHERE user_id = users.id),(SELECT ARRAY_AGG(users.profile_pic)AS profilepic FROM users WHERE user_id = users.id),(SELECT ARRAY_AGG(comments.user_id)AS commenter FROM comments WHERE posts.user_id = comments.post_id),(SELECT ARRAY_AGG(comments.body)AS comment FROM comments WHERE posts.user_id = comments.post_id),(SELECT ARRAY_AGG(comments.time_stamp)AS comment_time FROM comments WHERE posts.id = comments.id),(SELECT ARRAY_AGG(reactions.user_id)AS reactor FROM reactions WHERE posts.id = post_id),(SELECT ARRAY_AGG(reactions.reaction)AS reaction FROM reactions WHERE posts.id = post_id),(SELECT ARRAY_AGG(tags.tag)AS hash_tag FROM Tags WHERE posts.id = post_id) FROM posts"
     );
 
     res.status(200).json({
@@ -16,8 +18,8 @@ const getposts = async (req, res, next) => {
 
 const getpost = async (req, res, next) => {
   try {
-    let post = await dataBase.any(
-      "SELECT posts.id,posts.image, posts.brand, posts.description, posts.release_date,posts.colorway, posts.user_id, users.user_name FROM posts LEFT JOIN users ON posts.user_id = users.id WHERE user_id=$1",
+    let post = await db.any(
+      "SELECT *,(SELECT ARRAY_AGG(users.user_name)AS user_name FROM users WHERE user_id = users.id),(SELECT ARRAY_AGG(users.profile_pic)AS profilepic FROM users WHERE user_id = users.id),(SELECT ARRAY_AGG(comments.user_id)AS commenter FROM comments WHERE posts.user_id = comments.post_id),(SELECT ARRAY_AGG(comments.body)AS comment FROM comments WHERE posts.user_id = comments.post_id),(SELECT ARRAY_AGG(comments.time_stamp)AS comment_time FROM comments WHERE posts.id = comments.id),(SELECT ARRAY_AGG(reactions.user_id)AS reactor FROM reactions WHERE posts.id = post_id),(SELECT ARRAY_AGG(reactions.reaction)AS reaction FROM reactions WHERE posts.id = post_id),(SELECT ARRAY_AGG(tags.tag)AS hash_tag FROM Tags WHERE posts.id = post_id) FROM posts WHERE user_id=$1",
       [req.params.userId]
     );
     res.status(200).json({
@@ -32,7 +34,7 @@ const getpost = async (req, res, next) => {
 
 const newpost = async (req, res, next) => {
   try {
-    await dataBase.none(
+    await db.none(
       `INSERT INTO posts (user_id, image, brand, description, release_date, colorway) VALUES ('${req.body.user_id}','${req.body.image}','${req.body.brand}',${req.body.description},${req.body.release_date},${req.body.colorway} )`
     );
     res.status(200).json({
@@ -46,7 +48,7 @@ const newpost = async (req, res, next) => {
 
 const editPost = async (req, res, next) => {
   try {
-    let editedPost = await dataBase.any(
+    let editedPost = await db.any(
       `UPDATE posts SET body = '${req.body.description}' WHERE id=${req.params.id} RETURNING *`
     );
     res.status(200).json({
@@ -61,7 +63,7 @@ const editPost = async (req, res, next) => {
 
 const deletePost = async (req, res, next) => {
   try {
-    let deletedPost = await dataBase.any(
+    let deletedPost = await db.any(
       `DELETE FROM posts WHERE id = ${req.params.id} RETURNING *`
     );
     res.status(200).json({
