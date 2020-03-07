ebc4aab840c590d046902469d1382b47634a37e1
dannyl <danilovlaurent@pursuit.org>
2020-03-06 23:26:59 -0500

    create posts routes

--- backend/routes/posts/postRoutes.js
+++ backend/routes/posts/postRoutes.js
@@ -2,19 +2,19 @@ const posts = require("express").Router();
 const {
   getAllPosts,
   getPostById,
-  getTagOfPost,
-  getPostByTag,
-  getPicturesOfPost,
+  // getTagOfPost,
+  // getPostByTag,
+  // getPicturesOfPost,
   createPost,
   deletePost
 } = require("../../queries/posts/postQueries");
 
-// posts.get("/", getAllPosts);
-// posts.get("/:id", getPostById);
+posts.get("/", getAllPosts);
+posts.get("/:id", getPostById);
 // posts.get("/:id/:tag", getTagOfPost);
 // posts.get("/tags/:tagName", getPostByTag);
 // posts.get("/:id/pictures", getPicturesOfPost);
-// posts.post("/", createPost);
-// posts.delete("/:id", deletePost);
+posts.post("/", createPost);
+posts.delete("/:id", deletePost);
 
 module.exports = posts;
