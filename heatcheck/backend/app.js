ed783f9822a5b6cdb64dca1c4ea3c529ca5cdeb7
Syn Perez <perezsyn@gmail.com>
2020-03-07 11:13:17 -0500

    uncomment out app js

--- heatcheck/backend/app.js
+++ heatcheck/backend/app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser")
 
-// const postRouter = require('./routes/post')
+const postRouter = require('./routes/post')
 const commentRouter = require('./routes/comments')
-// const usersRouter = require("./routes/users.js")
-// const reactionsRouter = require('./routes/reactions')
+const usersRouter = require("./routes/users.js")
+const reactionsRouter = require('./routes/reactions')
 const tagsRouter = require("./routes/tags.js")
 
 const port = 3000;
@@ -16,10 +16,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-// app.use("/posts", postRouter)
+app.use("/posts", postRouter)
 app.use("/comments", commentRouter)
-// app.use("/users", usersRouter);
-// app.use("/reaction", reactionsRouter)
+app.use("/users", usersRouter);
+app.use("/reaction", reactionsRouter)
 app.use("/tags", tagsRouter)
 
 app.listen(port, () => {
