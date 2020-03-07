61df97ef8523a9c852404f4f16cd37f3958a5f91
bsihame <sihamebazi@pursuit.org>
2020-03-07 14:36:50 -0500

    create handleLogIn function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,20 @@ import { Route, Switch } from "react-router-dom";
 import SignInForm from "./components/LogIn/SignInForm";
 import SignUpForm from "./components/LogIn/SignUpForm";
 import Home from "./components/Home/Home";
-import Profile from "./components/Profile/Profile"
+import Profile from "./components/Profile/Profile";
 import "./App.css";
 
 function App() {
+  const handleLogIn = e => {
+    debugger;
+    //e.target.children[0].firstElementChild this will show the log-in value
+  };
+
   return (
     <div className="App">
       <Switch>
         <Route path={"/login"}>
-          <SignInForm />
+          <SignInForm handleLogIn={handleLogIn} />
         </Route>
         <Route path={"/signup"}>
           <SignUpForm />
@@ -20,7 +25,7 @@ function App() {
           <Profile />
         </Route>
         <Route exact path={"/"}>
-          <Home/>
+          <Home />
         </Route>
       </Switch>
     </div>
--- frontend/src/components/LogIn/SignInForm.js
+++ frontend/src/components/LogIn/SignInForm.js
@@ -1,24 +1,25 @@
 import React, { useState } from "react";
 import { useInput } from "../../util/customHooks";
 
-const SignInForm = () => {
+const SignInForm = ({ handleSubmit }) => {
   const email = useInput("");
-  const handleSubmit = e => {
-    e.preventDefault();
-  };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-      <label>
-        Email :
-        <input type="text" placeholder="Enter Your Email" {...email} />
-        <input type="submit" value="Sign In" />
-      </label>
-    </form>
-        <input type="button" value="Sign up" />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          handleSubmit(e);
+        }}
+      >
+        <label>
+          Email :
+          <input type="text" placeholder="Enter Your Email" {...email} />
+          <input type="submit" value="Sign In" />
+        </label>
+      </form>
+      <input type="button" value="Sign up" />
     </div>
-    
   );
 };
 export default SignInForm;
