c41a5586dd0033d02f5b7cae12715b11967e4aa3
bsihame <sihamebazi@pursuit.org>
2020-03-07 11:16:43 -0500

    add profile component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import SignInForm from "./components/LogIn/SignInForm"
-import SignUpForm from "./components/LogIn/SignUpForm"
-import Home from "./components/Home/Home"
-
-import './App.css';
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import SignInForm from "./components/LogIn/SignInForm";
+import SignUpForm from "./components/LogIn/SignUpForm";
+import Home from "./components/Home/Home";
+import Profile from "./components/Profile/Profile"
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <SignInForm/>
-      <SignUpForm/>
-      <Home/>
+      <Switch>
+        <Route path={"/login"}>
+          <SignInForm />
+        </Route>
+        <Route path={"/signup"}>
+          <SignUpForm />
+        </Route>
+        <Route path={"/profile"}>
+          <Profile />
+        </Route>
+        <Route exact path={"/"}>
+          <Home/>
+        </Route>
+      </Switch>
     </div>
   );
 }
