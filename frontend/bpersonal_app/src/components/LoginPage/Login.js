08f6c4354df8cbbdb9c3819f40c350c9cf6433bb
BrandonBrown <brandonbrown@pursuit.org>
2020-03-07 17:04:22 -0500

    saver

--- frontend/bpersonal_app/src/components/LoginPage/Login.js
+++ frontend/bpersonal_app/src/components/LoginPage/Login.js
@@ -1,17 +1,15 @@
 import React from "react";
 import LoginForm from './LoginForm'
 import SignUpForm from './SignUpForm'
-import {Switch} from 'react-router-dom'
+// import {Switch} from 'react-router-dom'
 
 
 function Login() {
     return (
         <div className="Login">
-            <Switch>
                 <LoginForm/>
+                
                 <SignUpForm/>
-            </Switch>
-
         </div>
     )
   }
