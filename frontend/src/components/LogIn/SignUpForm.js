91edd2fb1404879870e69d2424ef57a16a186e47
maria liza <mariamartinez@pursuit.org>
2020-03-07 17:56:06 -0500

    add css for signUp page

--- frontend/src/components/LogIn/SignUpForm.js
+++ frontend/src/components/LogIn/SignUpForm.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { useInput } from "../../util/customHooks";
+import OurCanvasLogo from "../../Images/OurCanvasLogo.jpg";
+import "../../CSS/signIn.css"
+// import OurCanvasBackground from "../../Images/OurCanvasBackground.jpg"
 
 const SignUpForm = () => {
   const email = useInput("");
@@ -12,34 +15,59 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSignUpSubmit} className="signUpForm">
-        <label>
-          Email :
-          <input type="text" placeholder="Enter Your Email" {...email} className="signUpEmail"/>
-        </label>
-        <label>
-          Genre :
-          <input type="text" placeholder="Enter Genre" {...typeOfArt} className="textInput signUpGenre"/>
-        </label>
-        <label>
-          Bio :
-          <input type="text" placeholder="Enter Your Bio" {...bio} className="textInput signUpBio"/>
-        </label>
-        <label>
-          Favorite Artist :
-          <input
-            type="text"
-            placeholder="Enter Your Favorite Artist"
-            className="textInput signUpFavArtist"
-            {...favoriteArtist}
-          />
-        </label>
+    <>
+      <div className="signUpFormDiv">
+        <div>
+          <img src={OurCanvasLogo} alt="" />
+        </div>
+        <form onSubmit={handleSignUpSubmit} className="signUpForm">
+          <label>
+            Email :
+            <input
+              type="text"
+              placeholder="Enter Your Email"
+              {...email}
+              className="signUpEmail"
+            />
+          </label>
+          <label>
+            Genre :
+            <input
+              type="text"
+              placeholder="Enter Genre"
+              {...typeOfArt}
+              className="textInput signUpGenre"
+            />
+          </label>
+          <label>
+            Bio :
+            <input
+              type="text"
+              placeholder="Enter Your Bio"
+              {...bio}
+              className="textInput signUpBio"
+            />
+          </label>
+          <label>
+            Favorite Artist :
+            <input
+              type="text"
+              placeholder="Enter Your Favorite Artist"
+              className="textInput signUpFavArtist"
+              {...favoriteArtist}
+            />
+          </label>
 
-        <input type="Submit" value="Sign Up" className="button signUpSubmit"/>
-      </form>
-      <input type="button" value="Log In" className="button signUpSwitch"/>
-    </div>
+          <input
+            type="Submit"
+            value="Sign Up"
+            className="button signUpSubmit"
+          />
+        </form>
+      
+        <input type="button" value="Log In" className="button signUpSwitch" />
+      </div>
+    </>
   );
 };
 export default SignUpForm;
