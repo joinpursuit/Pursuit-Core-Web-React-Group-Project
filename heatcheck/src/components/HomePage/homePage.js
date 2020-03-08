bc660220b1af67f28cdf0c046a17428b7fd3add0
Jay Fowler <jcafowler@gmail.com>
2020-03-08 12:29:58 -0400

    Started displaying buttons and login form

--- heatcheck/src/components/HomePage/homePage.js
+++ heatcheck/src/components/HomePage/homePage.js
@@ -1,11 +1,12 @@
 //home page
 import React, { Component } from 'react';
+import Login from "./login.js"
 
 class homePage extends Component {
   render() {
     return (
       <div>
-        
+        <Login/>
       </div>
     );
   }
