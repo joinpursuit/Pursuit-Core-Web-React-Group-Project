260a5ee1d399f0a12ab9744a24046383ed958107
Kevin Wong <kevinwong@pursuit.org>
2020-03-07 21:12:25 -0500

    fix database

--- backend/db/bpersonal_db.sql
+++ backend/db/bpersonal_db.sql
@@ -59,7 +59,7 @@ CREATE TABLE hashtags
 INSERT INTO users
     (username, password, full_name, email_address, profile_pic_url, bio)
 VALUES
-    ('kwong', 'yerradmin', 'Kevin Wong', 'kevinwong@pursuit.org', '‎⁨/uploads/IMAGE-flats1.jpg⁩', 'I''m kevin and this is my short bio'),
+    ('kwong', 'imag123', 'Kevin Wong', 'kevinwong@pursuit.org', '‎⁨https://pbs.twimg.com/profile_images/1150745933262577664/qOjN2JFT_400x400.jpg', 'I''m kevin and this is my short bio'),
     ('bbrown', 'secret', 'Brandon Brown', 'brandonbrown@pursuit.org', '/uploads/IMG_9504.jpg', 'This is brandons bio'),
     ('cannedBread', 'gang123', 'Marvin Thompson', 'marvinthompson@pursuit.org', '/uploads/83265522_3243496319013151_2072753519152070656_o.jpg', 'This is canned breads bio');
 
@@ -67,10 +67,10 @@ VALUES
 INSERT INTO posts
     (owner_id, post_image_url, body)
 VALUES
+
     (1, '/uploads/graduation.jpg', 'Blood, sweat, and tears. Graduation'),
-    (1, '‎⁨/uploads/snowboarding.jpg', 'Shredddin'),
-    (1, '‎⁨/uploads/yuki_waterfall.jpg⁩', 'Hiking with my Yuki'),
     (3, '/uploads/83265522_3243496319013151_2072753519152070656_o.jpg', 'Posted up on the stage'),
+    (1, 'https://pbs.twimg.com/profile_images/1007667023495745536/tsFzVI8-_400x400.jpg', 'Adventure in the canyons'),
     (3, '/uploads/62490637_2734115156617939_6992105001522823168_o.jpg', 'Posted up on this stool');
 
 INSERT INTO comments
@@ -90,35 +90,23 @@ VALUES
 INSERT INTO hashtags
     (owner_id, post_id, body)
 VALUES
-    (1, 1, 'saltflats'),
-    (1, 1, 'utah'),
-    (1, 1, 'adventure'),
-    (1, 2, 'graduation'),
-    (1, 2, 'album'),
-    (1, 3, 'doge'),
-    (1, 3, 'shiba'),
-    (1, 3, 'shibacam'),
-    (1, 4, 'oculus'),
-    (1, 5, 'snowboarding'),
-    (1, 5, 'shredding'),
-    (1, 6, 'desert'),
-    (1, 6, 'canyons'),
-    (1, 6, 'adventure'),
-    (1, 7, 'waterfall'),
-    (1, 7, 'shiba'),
-    (1, 7, 'doge'),
-    (1, 7, 'shibacam'),
-    (1, 7, 'adventure'),
-    (3, 1, 'posted'),
-    (3, 1, 'up'),
-    (3, 1, 'on'),
-    (3, 1, 'the'),
-    (3, 1, 'block'),
-    (3, 2, 'still'),
+
+    (1, 1, 'graduation'),
+    (1, 1, 'ye'),
+    (1, 1, 'album'),
+    (1, 3, 'adventures'),
+    (1, 3, 'desert'),
+    (1, 3, 'canyons'),
     (3, 2, 'posted'),
+    (3, 2, 'up'),
     (3, 2, 'on'),
     (3, 2, 'the'),
-    (3, 2, 'block');
+    (3, 2, 'block'),
+    (3, 4, 'still'),
+    (3, 4, 'posted'),
+    (3, 4, 'on'),
+    (3, 4, 'the'),
+    (3, 4, 'block');
 
 
 
