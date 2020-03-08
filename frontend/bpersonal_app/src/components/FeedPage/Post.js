6faf8894e281f2283afbb9fbea4d9a8fbf797d36
Kevin Wong <kevinwong@pursuit.org>
2020-03-07 22:28:09 -0500

    change postQuery to grab more data needed for users, change DisplayFeed and Post components to show readable data

--- frontend/bpersonal_app/src/components/FeedPage/Post.js
+++ frontend/bpersonal_app/src/components/FeedPage/Post.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const Post = ({ owner_id, post_image_url, body }) => {
+const Post = ({ full_name, timestamp, post_image_url, body }) => {
+  timestamp = new Date();
+  const date = timestamp.toDateString();
   return (
     <div>
-      Owner_id: {owner_id}
+      <h1>
+        {full_name} posted on {date}
+      </h1>
       <img src={post_image_url} alt={""}></img>
       <p>{body}</p>
     </div>
