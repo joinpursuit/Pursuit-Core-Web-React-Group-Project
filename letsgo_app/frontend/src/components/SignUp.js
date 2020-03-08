423e459fc53ab004f03c503c760dd1dfd449579a
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-03-08 12:04:30 -0400

    work on signup page

--- letsgo_app/frontend/src/components/SignUp.js
+++ letsgo_app/frontend/src/components/SignUp.js
@@ -14,6 +14,7 @@ const SignUp =()=> {
     
     const handleSumbit=async(e)=> {
         e.preventDefault();
+        
 
         const formData = new FormData();
         formData.append('myImage', file);
