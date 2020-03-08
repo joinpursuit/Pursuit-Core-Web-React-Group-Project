1f089a919488d6ad1ff36656b6a5c5a8fca83de9
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-03-08 19:52:38 -0400

    add isPostExisting middleware to getPictureOfPost and add in no received error check in catch

--- backend/routes/posts/postPictures/postPicturesRoutes.js
+++ backend/routes/posts/postPictures/postPicturesRoutes.js
@@ -1,6 +1,7 @@
 const postPicture = require("express").Router({ mergeParams: true });
 const getPicturesOfPost = require("../../../queries/posts/postPictures/postPicturesQueries");
+const { isPostExisting } = require("../../../queries/posts/postQueries");
 
-postPicture.get("/", getPicturesOfPost);
+postPicture.get("/", isPostExisting, getPicturesOfPost);
 
 module.exports = postPicture;
