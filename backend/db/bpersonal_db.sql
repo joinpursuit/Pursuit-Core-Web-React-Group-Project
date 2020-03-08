8710391f5f3d36c31c80e101cbe637c7b50b400b
Kevin Wong <kevinwong@pursuit.org>
2020-03-08 11:46:44 -0400

    database finally fixed, work on displayFeed hashtags

--- backend/db/bpersonal_db.sql
+++ backend/db/bpersonal_db.sql
@@ -59,7 +59,7 @@ CREATE TABLE hashtags
 INSERT INTO users
     (username, password, full_name, email_address, profile_pic_url, bio)
 VALUES
-    ('kwong', 'imag123', 'Kevin Wong', 'kevinwong@pursuit.org', 'https://lh3.googleusercontent.com/Fka0yDoXtaYHZBR9-mLtpCbHhTgGNRhRfh4E9qtVOBWMMgKspAFuz5Ejh5G0nAbzq0nJGVRaYTypltbr5abzM_bQCoUl8-neYemCqT5CybAjpdgx6nk70njf9tvzdU8jee7jEBdbsSLjEtnga2NyZnMBKs0x6Bnhb9_mujOfo9Tcd20xbn6PwrboAs5lsD28CvRupatH54SzHFr3Wm7EiLwcPdbPhCFeoWoudPxNsOoFtVQicAjaFEM8BXC48Rst_I8jvUQjeruCQx_oy2biT8jsrZ6eTvMR3yH0ferACS1a_YGa0doQ9HX5HGQFVEU1q1E5cwj0YeBjuSz3qZAscWoXSz0J0i0bMdWHwkIzG7PcEujxhsxXJL-jSNgYeXCg9oJI7TYTCHhAq5vui13TUQJP_p_sYxiATG6sc7H5FyVCMVaVs6tU_EF7nOm0MNWZNnrzOHNZ806Fl01lNFNlOLMQPajCV-zxb6eMof6e6GUUB59KzhFB2xSXFvBmyPe8o2CD1IAfvc9s5NupiUOHMNHwGDLWdhxXYggmDjug0HS6CAwAs2aWq0dyiLi3GzzSzWbMZdE5OtH2Jx04Xooj_YAj_Kna_mtb4Od8KeL7faWeWMH97H36iAFGXpDNW_Le9CYK2nma0j80yXbYPmO7iFynVBKef7jC9ccZ36NdlkdIaNj9UF89rfU=w119-h211-no', 'I''m kevin and this is my short bio'),
+    ('kwong', 'imag123', 'Kevin Wong', 'kevinwong@pursuit.org', 'uploads/saltflats.JPG', 'I''m kevin and this is my short bio'),
     ('bbrown', 'secret', 'Brandon Brown', 'brandonbrown@pursuit.org', '/uploads/IMG_9504.jpg', 'This is brandons bio'),
     ('cannedBread', 'gang123', 'Marvin Thompson', 'marvinthompson@pursuit.org', '/uploads/83265522_3243496319013151_2072753519152070656_o.jpg', 'This is canned breads bio');
 
