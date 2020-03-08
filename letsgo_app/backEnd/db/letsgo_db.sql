ed2beea811c2a76e6d4d863599921cc4d6fdec34
kongnyc <bbsyoow@gmail.com>
2020-03-08 16:31:02 -0400

    updated db with profile Pic

--- letsgo_app/backEnd/db/letsgo_db.sql
+++ letsgo_app/backEnd/db/letsgo_db.sql
@@ -33,10 +33,10 @@ CREATE TABLE Hashtags (
 );
 
 INSERT INTO Users (username, password, bio, profilePic, email)
-    VALUES ('darsu', 'admin123', 'Yuurrrrr', 'picurl','darsu@gmail.com' ),
-           ('henry', 'admin123', 'Hey party people, its ya boy', 'picurl','henry@gmail.com'),
-           ('sam', 'admin123', 'Heeeey now', 'picurl', 'sam@gmail.com'),
-           ('kong', 'admin123', 'PokemonGo ayeeeee', 'picurl', 'kong@gmail.com');
+    VALUES ('darsu', 'admin123', 'Yuurrrrr', 'https://ca.slack-edge.com/TCVA3PF24-UN3UXSZMY-e20128673daa-512','darsu@gmail.com' ),
+           ('henry', 'admin123', 'Hey party people, its ya boy', 'https://files.slack.com/files-pri/TCVA3PF24-FV2268GQY/screenshot_2020-01-14_at_4.35.20_pm.png','henry@gmail.com'),
+           ('sam', 'admin123', 'Heeeey now', 'https://files.slack.com/files-pri/TCVA3PF24-FV3TUKXC7/image_from_ios.jpg', 'sam@gmail.com'),
+           ('kong', 'admin123', 'PokemonGo ayeeeee', 'https://static1.squarespace.com/static/5b50ebb7e749401857e16f2f/t/5d7bbf0ef00cb05d84180599/1568390933661/CONGSONG%2C+YANG+-+Cong+Song+Yang.png', 'kong@gmail.com');
 
 INSERT INTO Posts (poster_id, imageURL, content)
     VALUES (1, '../../assets/singapore-skyline.jpg', 'Check me out in Singapore Skyline.'),
@@ -49,14 +49,25 @@ INSERT INTO Posts (poster_id, imageURL, content)
            (2, '../../assets/las-vegas.jpg', 'Views from Vegas');
 
 INSERT INTO Hashtags (poster_id, post_id, tag_name)
-    VALUES (1, 1, 'Montreal'),
+    VALUES (1, 1, 'Singapore'),
+           (1, 1, 'skyline'),
            (2, 2, 'runnersHigh'),
+           (2, 2, 'Boston'),
            (3, 3, 'France'),
+           (3, 3, 'CatchUp'),
            (3, 3, 'Europe'),
            (4, 4, 'New York City'),
            (4, 4, 'Pride'),
-           (3, 5, 'Cali'),
+           (3, 5, 'Rio'),
+           (3, 5, 'Brazil'),
            (1, 6, 'Bali'),
-           (4, 7, ' '),
+           (1, 6, 'Indonesia'),
+           (4, 7, 'MexicoCity'),
+           (4, 7, 'Mexico'),
+           (4, 7, 'Foodie'),
+           (4, 7, 'RealTacos'),
            (2, 8, 'Toronto'),
-           (2, 8, 'Canada');
+            (2, 8, 'Canada'),
+           (1, 9, 'Dubai'),
+           (1, 9, 'DinnerInTheSky'); 
+           
