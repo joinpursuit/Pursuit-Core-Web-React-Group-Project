45869dca6ff4d291d5362f6df23de9ecf5e4c85b
Kelvin <kelvinarellano@pursuit.org>
2020-03-08 12:31:56 -0400

    sign up for functionality started

--- heatcheck/src/components/HomePage/login.js
+++ heatcheck/src/components/HomePage/login.js
@@ -1,107 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+import SignUpForm from "./signupForm";
+import { Route,Switch } from "react-router-dom";
 
-class Login extends React.PureComponent {
-    state = {
-        email: '',
-        password: '',
-        username: '',
-        fullName: '',
-    }
+const LogIn = () => {
+    return(
+        <div className="LogInPage">
+            <Switch>
+                <SignUpForm/>
+            </Switch>
+        </div>
+    );
+};
 
-    handleFormSubmit = async (e) => {
-        e.preventDefault()
-
-        if (this.state.formFunctionality[0] === 'Login') {
-            const user = {
-                email: this.state.email,
-                password: this.state.password
-            }
-            try {
-                this.setState({loading: true})
-                const { data } = await axios.patch('http://localhost:3001/users/login', user)
-
-                this.setState({ loading: false })
-                this.props.formSubmit(data.payload, this.state.password)
-            } catch (err) {
-                
-            }
-        } else if (this.state.formFunctionality[0] === 'Sign-in') {
-            const user = { 
-                username: this.state.username, 
-                fullname: this.state.fullName, 
-                password: this.state.password, 
-                email: this.state.email
-            }
-            }
-            try {
-                this.setState({loading: true})
-                const { data } = await axios.post('http://localhost:3001/users/signup', user)
-
-                this.setState({ loading: false })
-                this.props.formSubmit(data.payload, this.state.password)
-            } catch (err) {
-            }
-        }
-    
-
-    handleUsernameInput = e => {
-        this.setState({username: e.target.value})
-    }
-
-
-    handlefullNameInput = e => {
-        this.setState({fullName: e.target.value})
-    }
-    
-    handleEmailInput = e => {
-        this.setState({email: e.target.value})
-    }
-
-    handlePasswordInput = e => {
-        this.setState({password: e.target.value})
-    }
-
-    handleSigninBtn = () => {
-        if (this.state.formFunctionality[0] === 'Login'){
-            this.setState({formFunctionality: ['Sign-in', 'Already a user? ', 'Login']})
-        } else if (this.state.formFunctionality[0] === 'Sign-in') {
-            this.setState({formFunctionality: ['Login', 'New user? ', 'Sign-in']})
-        }
-
-    }
-
-
-    render() {
-        if (this.state.formFunctionality[0] === 'Sign-in') {
-            signinFields = <>
-                <div>
-                    <input  id='username' type='text' value={this.state.username} onChange={this.handleUsernameInput} required></input>
-                </div>
-                <div >
-                    <input id='fullName' type='text' value={this.state.fullName} onChange={this.handlefullNameInput} required></input>
-                </div>
-            
-                <div >
-                    <input  id='defaultCheck' type='checkbox' checked={this.state.ageCheck} onChange={this.handleAgeCheck}/>
-                </div>
-            </>
-        }
-        return(
-            <form  onSubmit={this.handleFormSubmit} >
-                <div>
-                    <input id='email' type='email' value={this.state.email} onChange={this.handleEmailInput} required></input>
-                </div>
-                <div>
-                    <input  id='password' type='password' autoComplete='off' value={this.state.password} onChange={this.handlePasswordInput} required></input>
-                    <label >Password: </label>
-                </div>
-                {signinFields}
-                <div >
-                    <button onClick={this.handleSigninBtn}>{this.state.formFunctionality[1]}<strong>{this.state.formFunctionality[2]}</strong></button>
-                </div>
-            </form>
-        )
-    }
-}
-export default Login
+export default LogIn;
--- heatcheck/src/components/HomePage/signupForm.js
+++ heatcheck/src/components/HomePage/signupForm.js
@@ -1,4 +1,11 @@
-import React from "react"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+// const handleChange = e => {
+//     const [submit, setSubmit] = useState(false)
+//     e.preventDefault();
+
+// }
 
 const signUpForm = () => {
     return(
@@ -8,6 +15,7 @@ const signUpForm = () => {
         <input placeholder="Password"></input>
         <input placeholder="Email"></input>
         <input placeholder="Phone Number"></input>
+        <input type="file"></input>
         <button>Sign Up</button>
         </form>
         )
