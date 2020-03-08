9f23baf1ece78853a5ddd93aa0180de95d28237a
Kelvin <kelvinarellano@pursuit.org>
2020-03-08 18:17:45 -0400

    added functionality to buttons

--- heatcheck/src/App.js
+++ heatcheck/src/App.js
@@ -4,7 +4,7 @@ import HomePage from './components/HomePage/homePage'
 import Login from "./components/HomePage/login"
 import NavBar from "./components/Feedpage/nav_bar"
 import {Route, Switch} from 'react-router-dom'
-
+//
 function App() {
   return (
     <div className="App">
