5e6e205d7c41ba4f4e2ed09912843faee38b0c44
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-03-08 20:14:32 -0400

    finish getTagByName query that sends back all tags matching the name entered as a param

--- backend/queries/tags/tagsQueries.js
+++ backend/queries/tags/tagsQueries.js
@@ -14,7 +14,23 @@ const getAllTags = async (req, res, next) => {
 };
 
 const getTagByName = async (req, res, next) => {
-
+  const { tag } = req.params;
+  try {
+    let posts = await db.any(`SELECT *, tags.id AS id, posts.id AS post_id, users.id AS poster_id FROM tags 
+                              LEFT JOIN posts ON posts.id=tags.post_id 
+                              LEFT JOIN users ON users.id=posts.poster_id WHERE tag=$1`, tag);
+    if(posts.length) {
+      res.status(200).json({
+        status: "ok",
+        posts,
+        message: "Retrieved all posts by search"
+      })
+    } else {
+      throw {status: 404, error: `No tags found by search: ${tag}`}
+    }
+  } catch(error) {
+    next(error);
+  }
 }
 
 const createTag = async (req, res, next) => {
