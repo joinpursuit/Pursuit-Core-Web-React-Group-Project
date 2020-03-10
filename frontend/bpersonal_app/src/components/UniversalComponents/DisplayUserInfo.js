cf6f0be37118dd4892c489a342ad3b07d4cace9b
Wilsantos1975 <faustosantos@pursuit.org>
2020-03-09 21:53:09 -0400

    added email to user info

--- frontend/bpersonal_app/src/components/UniversalComponents/DisplayUserInfo.js
+++ frontend/bpersonal_app/src/components/UniversalComponents/DisplayUserInfo.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Avatar from 'react-avatar'
+import Avatar from "react-avatar";
 
 const DisplayUserInfo = () => {
   const [user, setUser] = useState({});
+  const[editBoolean, setEditBoolean] = useState(false);
 
   const fetchData = async url => {
     try {
       let res = await axios.get(url);
-    //   debugger;
+      //   debugger;
       const { single_user } = res.data.body;
       setUser(single_user);
     } catch (error) {
@@ -16,16 +17,37 @@ const DisplayUserInfo = () => {
       console.log(error);
     }
   };
+
+const handleClick = () => {
+    if (editBoolean){
+        // style.display of the form "block"
+    } else {
+        // style.display of the form "none"
+    }
+}
+  
+
+
   useEffect(() => {
     fetchData("users/1");
   }, []);
 
   return (
     <section id="displayUser">
-      <img src={user.profile_pic_url}></img>
+      <img
+        src={user.profile_pic_url}
+        style={{ width: "300px", height: "300px", borderRadius: "100%" }}
+      ></img>
       <h1> {user.full_name} </h1>
       <h2>{user.username}</h2>
       <h2>{user.bio}</h2>
+      <h2>{user.email}</h2>
+      <form id="updateBio">
+          <input id="name" type="text"/>
+          <input id="userName" type="text"/>
+          <input id="bio" type="text"/>
+      </form>
+      <button onClick={handleCLick}>Edit Profile</button>
     </section>
   );
 };
