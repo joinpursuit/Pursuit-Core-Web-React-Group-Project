5e317ec9d72c81272c810653819b019ed107087a
Syn Perez <perezsyn@gmail.com>
2020-03-09 22:03:53 -0400

    treading tags and uploading work

--- heatcheck/backend/queries/tags.js
+++ heatcheck/backend/queries/tags.js
@@ -17,6 +17,23 @@ const getAllTags = async (req, res, next) => {
   }
 }
 
+const trendingTags = async (req, res, next) => {
+  try {
+    let tags = await db.any ("SELECT tags.tag, COUNT(tags.tag) FROM tags GROUP BY tags.tag ORDER BY COUNT(*) DESC LIMIT 5")
+    res.status(200).json({
+      status: "success",
+      message: "Retrieved all tags.",
+      payload: tags
+    })
+  } catch(err){
+    res.status(404).json({
+      status: "error",
+      message: "There were no tags found.",
+      payload: null
+    })
+  }
+}
+
 const getTagsByName = async (req, res, next) => {
   try{
     let {tagName} = req.params
@@ -79,4 +96,4 @@ const deleteTag = async (req, res, next) => {
   }
 }
 
-module.exports = {getAllTags, getTagsByName, postTag, deleteTag}
+module.exports = {getAllTags, trendingTags, getTagsByName, postTag, deleteTag}
