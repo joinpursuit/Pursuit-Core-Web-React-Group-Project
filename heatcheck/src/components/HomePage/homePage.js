b334a525a27cec3d6e3a894bc4ceeb83fb3ec6a8
Jay Fowler <jcafowler@gmail.com>
2020-03-11 00:36:53 -0400

    Created forms, need to link refs

--- heatcheck/src/components/HomePage/homePage.js
+++ heatcheck/src/components/HomePage/homePage.js
@@ -1,14 +1,62 @@
 //home page
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import Login from "./login.js"
+import SignUp from "./signup.js"
+import "../../css/homePage.scss"
 
-class homePage extends Component {
-  render() {
-    return (
-      <div>
+const RightSide = (props) => {
+  return (
+    <div className="right-side" ref={props.containerRef} onClick={props.onClick}>
+      <div className="inner-container">
+        <div className="text">{props.active}Hi</div>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-export default homePage;
+
+const HomePage = () => {
+  const [isLoginActive, setIsLoginActive] = useState(true)
+  const [current, setCurrent] = useState()
+  const [container, setContainer] = useState()
+  const [rightSide, setRightSide] = useState()
+  const active = isLoginActive ? "Sign Up" : "Login"
+  const containerRef = useRef()
+
+  const changeState = () => {
+    if(isLoginActive){
+      rightSide.classList.remove("right")
+      rightSide.classList.add("left")
+    } else {
+      rightSide.classList.remove("left")
+      rightSide.classList.add("right")
+    }
+    setIsLoginActive(!isLoginActive)
+  }
+
+  useEffect(() => {
+    setRightSide("right")
+  },[])
+
+  return(
+    <div className="HomePage">
+      <div className="login">
+        <div className="container" ref={(ref) => {
+          setContainer(ref) 
+        return container} }>
+          {isLoginActive && (<Login containerRef={(ref) => {
+            setCurrent(ref)
+            return current}}/>)}
+          {!isLoginActive && (<SignUp containerRef={(ref) => {
+            setCurrent(ref)
+            return current}}/>)}
+        </div>
+        <RightSide active={active} containerRef={(ref) => {
+          setRightSide(ref)
+          return rightSide}} onClick={changeState()}/>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage;
