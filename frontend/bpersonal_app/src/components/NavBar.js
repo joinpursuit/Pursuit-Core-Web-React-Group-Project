6fb04c99aecefab217db17c4555155b79744e5f9
Marvin Thompson <marvinthompson@pursuit.org>
2020-03-10 22:50:49 -0400

    start searchResultsComponent

--- frontend/bpersonal_app/src/components/NavBar.js
+++ frontend/bpersonal_app/src/components/NavBar.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav>
-      {/* <form className={"search"}>
+      <form className={"search"}>
         <input placeholder={"Search for hashtags here !"}></input>
         <Link to={"/results"}>
           <button>SEARCH ME</button>
         </Link>
-      </form> */}
+      </form>
       <NavLink to={"/feedpage"}>FEED</NavLink>
       <NavLink exact to={"/profilepage"}>
         PROFILE
