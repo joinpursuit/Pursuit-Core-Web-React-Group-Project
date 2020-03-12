8b32fcd48ab032d332b77b5611f933fd2def8612
dannyl <danilovlaurent@pursuit.org>
2020-03-11 22:06:47 -0400

    complete post component

--- frontend/src/components/PhotoUpLoad/photoUpLoad.js
+++ frontend/src/components/PhotoUpLoad/photoUpLoad.js
@@ -1,32 +1,41 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useInput } from "../../util/customHooks";
 
-const UploadImage = () => {
+const UploadImage = ({ userId }) => {
   const [file, setFile] = useState();
+  const caption = useInput();
 
   const onSubmit = async e => {
-    e.preventDefault();
-    const formData = new FormData();
+    try {
+      e.preventDefault();
+      const formData = new FormData();
 
-    formData.append("myImage", file);
-    formData.append("post_id", 2);
-    console.log(file);
+      formData.append("myImage", file);
+      formData.append("poster_id", userId);
+      formData.append("caption", caption.value);
+      formData.append("created_at", new Date().toString());
 
-    const config = {
-      headers: {
-        "content-type": "multipart/form-data"
-      }
-    };
-    let res = await axios.post("/api/pictures", formData, config);
+      const config = {
+        headers: {
+          "content-type": "multipart/form-data"
+        }
+      };
+      let res = await axios.post("/api/posts", formData, config);
+    } catch (err) {
+      console.log(err);
+    }
   };
   const onChange = e => {
     setFile(e.target.files[0]);
   };
+  console.log(caption);
 
   return (
     <form onSubmit={onSubmit}>
       <h1>File Upload</h1>
       <input type="file" name="myImage" onChange={onChange} />
+      <input type="text" {...caption}></input>
       <input type="submit"></input>
     </form>
   );
