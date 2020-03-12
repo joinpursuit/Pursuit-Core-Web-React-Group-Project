6ab8802810265967cdde139b5710438a506e0217
Kevin Wong <kevinwong@pursuit.org>
2020-03-11 21:29:24 -0400

    Post component working, work on hashtags access

--- frontend/bpersonal_app/src/components/FeedPage/CreatePostForm.js
+++ frontend/bpersonal_app/src/components/FeedPage/CreatePostForm.js
@@ -5,29 +5,35 @@ import { useInput } from "../../util/customHooks";
 
 const CreatePostForm = () => {
   const captionObj = useInput("");
+  const captionInput = captionObj.value;
 
-  const handleCreatePostClick = async () => {
+  const [path, setPath] = useState("");
+
+  const handleCreatePost = async () => {
     try {
-      let res = await axios.post("/posts/");
+      let res = await axios.post("/posts/", {
+        owner_id: sessionStorage.userID,
+        post_image_url: path,
+        body: captionInput
+      });
     } catch (error) {
       console.log(error);
     }
   };
 
-  console.log(captionObj);
   return (
     <div className="createPost">
       <h1>Create a post</h1>
-      <Upload />
-      <input
-        name={"body"}
-        {...captionObj}
-        type="text"
-        placeholder="Enter a caption"
-      ></input>
-      <button type="click" onClick={handleCreatePostClick}>
-        Create post
-      </button>
+      <Upload cb={setPath} />
+      <form onSubmit={handleCreatePost}>
+        <input
+          name={"body"}
+          type="text"
+          {...captionObj}
+          placeholder="Enter a caption"
+        ></input>
+        <button type="click">Create post</button>
+      </form>
     </div>
   );
 };
--- frontend/bpersonal_app/src/components/UniversalComponents/DisplayUserInfo.js
+++ frontend/bpersonal_app/src/components/UniversalComponents/DisplayUserInfo.js
@@ -1,70 +1,70 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { useInput, useToggleShow } from '../../util/customHooks'
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import { useInput, useToggleShow } from '../../util/customHooks'
 
-const DisplayUserInfo = () => {
-  const [user, setUser] = useState({});
-  const [editBoolean, setEditBoolean] = useState(false);
-//   const editProfileInputObj = useInput("")
-  const toggleEditProfile = useToggleShow(false);
+// const DisplayUserInfo = () => {
+//   const [user, setUser] = useState({});
+//   const [editBoolean, setEditBoolean] = useState(false);
+// //   const editProfileInputObj = useInput("")
+//   const toggleEditProfile = useToggleShow(false);
 
-  const fetchData = async url => {
-    try {
-      let res = await axios.get(url);
-      //   debugger;
-      const { single_user } = res.data.body;
-      setUser(single_user);
-    } catch (error) {
-      setUser({});
-      console.log(error);
-    }
-  };
+//   const fetchData = async url => {
+//     try {
+//       let res = await axios.get(url);
+//       //   debugger;
+//       const { single_user } = res.data.body;
+//       setUser(single_user);
+//     } catch (error) {
+//       setUser({});
+//       console.log(error);
+//     }
+//   };
 
-  const handleUpdateProfile = async e => {
-    e.preventDefault();
-    await axios.patch(`users/${sessionStorage.userID}`, {
-      userName: username,
-      name: full_name,
-      bio: bio,
-      email: email
-    });
-  };
+//   const handleUpdateProfile = async e => {
+//     e.preventDefault();
+//     await axios.patch(`users/${sessionStorage.userID}`, {
+//       userName: username,
+//       name: full_name,
+//       bio: bio,
+//       email: email
+//     });
+//   };
 
-  // const handleClick = () => {
-  //     if (editBoolean){
-  //         // style.display of the form "block"
-  //     } else {
-  //         // style.display of the form "none"
-  //     }
-  // }
+//   // const handleClick = () => {
+//   //     if (editBoolean){
+//   //         // style.display of the form "block"
+//   //     } else {
+//   //         // style.display of the form "none"
+//   //     }
+//   // }
 
-  useEffect(() => {
-    fetchData("users/1");
-  }, []);
+//   useEffect(() => {
+//     fetchData("users/1");
+//   }, []);
 
-  return (
-    <section id="displayUser">
-      <img
-        src={user.profile_pic_url}
-        style={{ width: "300px", height: "300px", borderRadius: "100%" }}
-      ></img>
-      <h1>{user.username}:</h1>
-      <h2> {user.full_name}</h2>
-      <h2>{user.bio}</h2>
-      <h2>{user.email}</h2>
-      <button id="editProfileButton" onClick={toggleEditProfile.onclick} >Edit Profile</button>
-      
-      {toggleEditProfile.showInsert ? (
-      <form id="updateBio" onSubmit={handleUpdateProfile}>
-        <input id="name" type="text" />
-        <input id="userName" type="text" />
-        <input id="email" type="text" />
-        <input id="bio" type="text" />
-      <button>Edit Profile</button>
-      </form>
-      ) : null}
-    </section>
-  );
-};
+//   return (
+//     <section id="displayUser">
+//       <img
+//         src={user.profile_pic_url}
+//         style={{ width: "300px", height: "300px", borderRadius: "100%" }}
+//       ></img>
+//       <h1>{user.username}:</h1>
+//       <h2> {user.full_name}</h2>
+//       <h2>{user.bio}</h2>
+//       <h2>{user.email}</h2>
+//       <button id="editProfileButton" onClick={toggleEditProfile.onclick} >Edit Profile</button>
 
-export default DisplayUserInfo;
+//       {toggleEditProfile.showInsert ? (
+//       <form id="updateBio" onSubmit={handleUpdateProfile}>
+//         <input id="name" type="text" />
+//         <input id="userName" type="text" />
+//         <input id="email" type="text" />
+//         <input id="bio" type="text" />
+//       <button>Edit Profile</button>
+//       </form>
+//       ) : null}
+//     </section>
+//   );
+// };
+
+// export default DisplayUserInfo;
