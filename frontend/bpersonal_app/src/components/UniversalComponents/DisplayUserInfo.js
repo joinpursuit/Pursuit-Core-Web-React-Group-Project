e48d5fc12ddc2ae24b0213c64e1ce8fc75c8ef7b
Wilsantos1975 <faustosantos@pursuit.org>
2020-03-12 10:10:11 -0400

    check errors

--- frontend/bpersonal_app/src/components/UniversalComponents/DisplayUserInfo.js
+++ frontend/bpersonal_app/src/components/UniversalComponents/DisplayUserInfo.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useInput, useToggleShow } from '../../util/customHooks'
+import { useInput, useToggleShow } from "../../util/customHooks";
 
 const DisplayUserInfo = () => {
   const [user, setUser] = useState({});
   const [editBoolean, setEditBoolean] = useState(false);
-//   const editProfileInputObj = useInput("")
+  const editProfileInputObj = useInput("");
   const toggleEditProfile = useToggleShow(false);
 
   const fetchData = async url => {
@@ -23,10 +23,10 @@ const DisplayUserInfo = () => {
   const handleUpdateProfile = async e => {
     e.preventDefault();
     await axios.patch(`users/${sessionStorage.userID}`, {
-    //   userName: username,
-    //   name: full_name,
-    //   bio: bio,
-    //   email: email
+      //   userName: username,
+      //   name: full_name,
+      //   bio: bio,
+      //   email: email
     });
   };
 
@@ -52,16 +52,18 @@ const DisplayUserInfo = () => {
       <h2> {user.full_name}</h2>
       <h2>{user.bio}</h2>
       <h2>{user.email}</h2>
-      <button id="editProfileButton" onClick={toggleEditProfile.onclick} >Edit Profile</button>
-      
+      <button id="editProfileButton" onClick={toggleEditProfile.onclick}>
+        Edit Profile
+      </button>
+
       {toggleEditProfile.showInsert ? (
-      <form id="updateBio" onSubmit={handleUpdateProfile}>
-        <input id="name" type="text" />
-        <input id="userName" type="text" />
-        <input id="email" type="text" />
-        <input id="bio" type="text" />
-      <button>Edit Profile</button>
-      </form>
+        <form id="updateBio" onSubmit={handleUpdateProfile}>
+          <input id="name" type="text" />
+          <input id="userName" type="text" />
+          <input id="email" type="text" />
+          <input id="bio" type="text" />
+          <button>Edit Profile</button>
+        </form>
       ) : null}
     </section>
   );
