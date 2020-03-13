556512a6500f7dccfdf07e7f9795a2071f94bd08
Marvin Thompson <marvinthompson@pursuit.org>
2020-03-12 20:34:29 -0400

    almost finished results

--- frontend/bpersonal_app/src/components/ResultsPage/Results.js
+++ frontend/bpersonal_app/src/components/ResultsPage/Results.js
@@ -1,13 +1,25 @@
-import React from 'react';
-// import SearchHashtags from './SearchHashtags'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-const Results = () => {
+const Results = ({searchInput}) => {
+    const [allHashtagPosts, setAllHashtagPosts] = useState([])
+
+    const fetchAllHashtagPosts = async (url) => {
+            try {
+              let res = await axios.get(url)
+              debugger
+            } catch (error) {
+              console.log(error)  
+            }
+    }
+
+    useEffect(() => {
+        fetchAllHashtagPosts(`/posts/${searchInput}`)
+    }, [])
     return (
-        <>
-            <div>
-            {/* <SearchHashtags /> */}
-            </div>
-        </>
+        <div className={"displayResults"}>
+        <button>Go Back</button>
+        </div>
     )
 }
 
