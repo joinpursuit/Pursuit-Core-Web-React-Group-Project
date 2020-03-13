e96977a86d2839e0fe980bd2b40471094b5d51a3
Wilsantos1975 <faustosantos@pursuit.org>
2020-03-12 20:34:23 -0400

    displayUserInfo working

--- frontend/bpersonal_app/src/components/UniversalComponents/DisplayUserInfo.js
+++ frontend/bpersonal_app/src/components/UniversalComponents/DisplayUserInfo.js
@@ -6,12 +6,13 @@ const DisplayUserInfo = () => {
   const [user, setUser] = useState({});
   const toggleEditProfile = useToggleShow(false);
   const userNameObj = useInput("");
-  const userNameInput = userNameObj.value;
   const fullNameObj = useInput("");
   const bioObj = useInput("");
   const emailObj = useInput("");
-  //   const [editBoolean, setEditBoolean] = useState(false);
-  //   const editProfileInputObj = useInput("");
+  const userNameInput = userNameObj.value;
+  const fullName = fullNameObj.value;
+  const bio = bioObj.value;
+  const email = emailObj.value;
 
   const fetchData = async url => {
     try {
@@ -28,10 +29,10 @@ const DisplayUserInfo = () => {
   const handleUpdateProfile = async e => {
     e.preventDefault();
     await axios.patch(`users/${sessionStorage.userID}`, {
-      userName: userNameInput
-      // name: full_name,
-      // bio: bio,
-      // email: email
+      userName: userNameInput,
+      name: fullName, 
+      bio: bio,
+      email: email
     });
   };
 
@@ -58,17 +59,17 @@ const DisplayUserInfo = () => {
       <h2> {user.full_name}</h2>
       <h2>{user.bio}</h2>
       <h2>{user.email}</h2>
-      <button id="editProfileButton" onClick={toggleEditProfile.onclick}>
+      <button id="editProfileButton" onClick={toggleEditProfile.onClick}>
         Edit Profile
       </button>
 
       {toggleEditProfile.showInsert ? (
         <form id="updateBio" onSubmit={handleUpdateProfile}>
-          <input name={"full_name"} {...fullNameObj} type="text" />
-          <input name={"userName"} {...userNameObj} type="text" />
-          <input name={"email"} {...emailObj} type="text" />
-          <input name={"bio"} {...bioObj} type="text" />
-          <button>Edit Profile</button>
+          <input name={"full_name"} {...fullNameObj} type="text" placeholder="name"/>
+          <input name={"userName"} {...userNameObj} type="text" placeholder="username"/>
+          <input name={"email"} {...emailObj} type="text" placeholder="email"/>
+          <input name={"bio"} {...bioObj} type="text" placeholder="bio"/>
+          <button type="submit" >Edit Profile</button>
         </form>
       ) : null}
     </section>
