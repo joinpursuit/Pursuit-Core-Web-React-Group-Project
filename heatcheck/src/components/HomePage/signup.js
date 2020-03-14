8bf324665d2c6ecfb1b405a499e6f06af9a64190
Jay Fowler <jcafowler@gmail.com>
2020-03-14 15:53:19 -0400

    Add function to make axios requests on button click for form

--- heatcheck/src/components/HomePage/signup.js
+++ heatcheck/src/components/HomePage/signup.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import axios from "axios"
+import {useInput} from "../../util/customHooks.js"
 import loginIcon from "./signIn.svg"
 import "../../css/form.scss"
 
 const SignUp = (props) => {
+  const newFullName = useInput("")
+  const newUsername = useInput("")
+  const newEmail = useInput("")
+  const newPassword = useInput("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let full_name = newFullName.value;
+      let user_name = newUsername.value;
+      let email = newEmail.value;
+      let password = newPassword.value;
+      await axios.post("http://localhost:3001/users",{full_name, email, user_name, password});
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return(
     <div className="base-container signup" ref={props.containerRef}>
       <div className="header">Sign Up</div>
@@ -13,24 +33,24 @@ const SignUp = (props) => {
         <div className="form">
           <div className="form-group">
             <label htmlFor="fullName">Full Name</label>
-            <input type="text" name="fullName" placeholder="Full Name" required/>
+            <input type="text" name="fullName" placeholder="Full Name" {...newFullName} required/>
           </div>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input type="text" name="username" placeholder="Username" required/>
+            <input type="text" name="username" placeholder="Username" {...newUsername} required/>
           </div>
           <div className="form-group">
             <label htmlFor="e-mail">E-mail</label>
-            <input type="text" name="e-mail" placeholder="E-mail" required/>
+            <input type="text" name="e-mail" placeholder="E-mail" {...newEmail} required/>
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" placeholder="Password" required/>
+            <input type="password" name="password" placeholder="Password" {...newPassword} required/>
           </div>
         </div>
       </div>
       <div className="footer">
-        <button type="button" className="btn">Sign Up</button>
+        <button type="button" className="btn" onClick={handleSubmit}>Sign Up</button>
       </div>
     </div>
   )
