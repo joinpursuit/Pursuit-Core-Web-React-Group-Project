3ebb40fcbae314aa0cbe30c163a76dc30ebe5b49
Kelvin <kelvinarellano@pursuit.org>
2020-03-13 23:18:01 -0400

    navbar reconfig

--- heatcheck/src/App.js
+++ heatcheck/src/App.js
@@ -4,17 +4,12 @@ import HomePage from './components/HomePage/homePage'
 import Login from "./components/HomePage/login"
 import NavBar from "./components/Feedpage/nav_bar"
 import {Route, Switch} from 'react-router-dom'
-//
+import UploadPost from './components/Feedpage/uploadPost'
+
 function App() {
   return (
     <div className="App">
-      <div></div>
-     <HomePage/>
-     <Login/>
-     <NavBar/>
-
-     <feedpage/>
-      {/* //<Posts /> */}
+     <UploadPost/>
     </div>
   );
 }
